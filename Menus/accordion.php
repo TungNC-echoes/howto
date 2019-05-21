5eae4adf34c1968585ed1117c6232f754cdfd120
TungDEPZAI <tungnc@kaopiz.com>
2019-05-21 15:17:54 +0700

    -a

---
+++ Menus/accordion.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ACCORDION</title>
+    <link rel="stylesheet" href="../css/Menus/accordion.css">
+</head>
+<body>
+<button class="accordion">Section 1</button>
+<div class="panel">
+    <p>TUNGNC1</p>
+</div>
+
+<button class="accordion">Section 2</button>
+<div class="panel">
+    <p>TUNGNC2</p>
+</div>
+
+<button class="accordion">Section 3</button>
+<div class="panel">
+    <p>TUNGNC3</p>
+</div>
+<script src="../js/Menus/accordion.js"></script>
+</body>
+</html>
